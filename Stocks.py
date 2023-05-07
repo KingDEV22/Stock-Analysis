fbd00e326e821ab7efee54a677aafaa11f8e0207
sayantan1413 <sayantanbose2001@gmail.com>
2023-05-08 00:51:47 +0530

    Formatted code

--- Stocks.py
+++ Stocks.py
@@ -22,28 +22,28 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'  # Set the date and time format
 )
 
-
-mcnews = []
-mctimes = []
-mnews = []
-mtimes = []
-bsnews = []
-bstimes = []
-ecnews = []
-ectimes = []
+#Global variables 
+money_control_news = []
+money_control_datetimes = []
+mint_news = []
+mint_datetimes = []
+business_standard_news = []
+business_standard_datetimes = []
+economic_times_news = []
+economic_times_datetimes = []
 
 
 today = date.today() - timedelta(1)
 today = str(today) + ' 09:00 PM'
 stop_date = datetime.strptime(today, '%Y-%m-%d %I:%M %p')
 
-
+# Methods declared
 def fetch_from_url(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
-    logging.info("fetch from " + url)
+    logging.info("Fetch from " + url)
     return soup
 
 
@@ -52,85 +52,83 @@ def extract_data_from_page(page, ele, classname):
 
 
 def economic_news():
-    today = datetime.now()
-    month = today.strftime("%b")
-    logging.info("fetching from economic times")
-    for x in range(1, 4):
+    logging.info("Extracting data from economic times news")
+    for page_no in range(1, 4):
         url = "https://economictimes.indiatimes.com/markets/stocks/news/articlelist/msid-2146843,"
-        page = fetch_from_url(url + "page-" + str(x) + ".cms")
-        result = extract_data_from_page(page, 'div', 'eachStory')
-        for i in result:
-            data = i.find('a')
-            times = i.find('time')
+        page = fetch_from_url(url + "page-" + str(page_no) + ".cms")
+        results = extract_data_from_page(page, 'div', 'eachStory')
+        for result in results:
+            data = result.find('a')
+            times = result.find('time')
             times = times.text
             stop_time = datetime.strptime(
                 str(times).strip(), '%b %d, %Y, %I:%M %p IST')
             data = data.text.strip()
             if stop_time <= stop_date:
-                logging.info("fetching comepleted from economic times")
+                logging.info("Extraction comepleted from economic times news")
                 return
 
-            ecnews.append(data)
-            ectimes.append(format_date(stop_time))
+            economic_times_news.append(data)
+            economic_times_datetimes.append(format_date(stop_time))
 
 
 def mint_news():
-    logging.info("fetching from mint")
-    for x in range(1, 4):
+    logging.info("Extracting data from mint news")
+    for page_no in range(1, 4):
         url = "https://www.livemint.com/market/stock-market-news/"
-        page = fetch_from_url(url+"page-" + str(x))
+        page = fetch_from_url(url+"page-" + str(page_no))
 
-        result = extract_data_from_page(page, 'div', 'headlineSec')
-        for i in result:
-            data = i.find('a')
-            tim = i.find('span')
+        results = extract_data_from_page(page, 'div', 'headlineSec')
+        for result in results:
+            data = result.find('a')
+            tim = result.find('span')
 
             data = data.text.strip()
             tim = "".join(
-                [str(x)[136:161] for x in i.contents if "<span data-expandedtime" in str(x)])
+                [str(x)[136:161] for x in result.contents if "<span data-expandedtime" in str(x)])
 
             stop_time = datetime.strptime(str(tim), '%d %b %Y, %I:%M %p IST')
             if stop_time <= stop_date:
-                logging.info("fetching completed from mint")
+                logging.info("Extraction completed from mint news")
                 return
-            mnews.append(data)
-            mtimes.append(format_date(stop_time))
+            mint_news.append(data)
+            mint_datetimes.append(format_date(stop_time))
 
 
 def money_control_news():
-    logging.info("fetching from money control")
-    for x in range(1, 4):
+    logging.info("Extracting data from money control news")
+    for page_no in range(1, 4):
         url = "https://www.moneycontrol.com/news/business/markets/"
-        page = fetch_from_url(url+"page-" + str(x))
+        page = fetch_from_url(url+"page-" + str(page_no))
 
-        result = extract_data_from_page(page, 'li', 'clearfix')
-        for i in result:
+        results = extract_data_from_page(page, 'li', 'clearfix')
+        for result in results:
             stop_time = ''
-            page_text = (i.get_text()).strip()
+            page_text = (result.get_text()).strip()
             page_text = str(page_text).replace(
-                i.find('p').text.strip(), "").strip()
+                result.find('p').text.strip(), "").strip()
 
             page_text = page_text.split('IST')
             stop_time = datetime.strptime(
                 str(page_text[0]).strip(), '%B %d, %Y %I:%M %p')
             if stop_time <= stop_date:
-                logging.info("fetching completed from money control")
+                logging.info("Extraction completed from money control news")
                 return
-            mcnews.append(page_text[1])
-            mctimes.append(format_date(stop_time))
+            money_control_news.append(page_text[1])
+            money_control_datetimes.append(format_date(stop_time))
 
 
 def business_standard_news():
     date_stop = datetime.now().date()
-    logging.info("fetching from business standard")
+    logging.info("Extracting data from business standard news")
     url = "https://www.business-standard.com/markets/news"
     page = fetch_from_url(url)
-    result = extract_data_from_page(page, 'div', 'cardlist')
-    for i in result:
-        data = i.find('a')
-        bsnews.append(data.text.strip())
-        bstimes.append(date_stop)
-    logging.info("fetching completed from business standard")
+    results = extract_data_from_page(page, 'div', 'cardlist')
+    for result in results:
+        data = result.find('a')
+        business_standard_news.append(data.text.strip())
+        business_standard_datetimes.append(date_stop)
+    logging.info("Extraction completed from business standard news")
 
 
 def text_data_cleaning(sentence):
@@ -163,7 +161,7 @@ def filter_data(data):
     data = data[data['Text'].str.contains("\?") != True]
     data = data[data['Text'].str.contains("Wall Street|Amazon|Google") != True]
     data['Tok_text'] = data['Text'].apply(text_data_cleaning)
-    logging.info("stopwords removed and lematization completed")
+    logging.info("Stopwords removed and Lematization completed")
     return data
 
 
@@ -171,13 +169,13 @@ def ml_model(data):
     model = pickle.load(open('./pkl/model.pkl', 'rb'))
     model1 = pickle.load(open('./pkl/model1.pkl', 'rb'))
     tfidf = pickle.load(open('./pkl/vectorizer.pkl', 'rb'))
-    logging.info("model loaded")
+    logging.info("Model loaded")
     x_test = tfidf.transform(data['Tok_text'])
     y_pred = model.predict(x_test)
-    logging.info("data predicted")
+    logging.info("Data predicted")
     data['sdg_pred'] = y_pred
     y_pred1 = model1.predict(x_test)
-    logging.info("data predicted")
+    logging.info("Data predicted")
     data['lr_pred'] = y_pred1
     return data
 
@@ -188,20 +186,20 @@ def dp_model(data):
     sequences = tokenizer.texts_to_sequences(data['Tok_text'])
     padded_sequences = pad_sequences(sequences, maxlen=max_length)
     lstm_model = load_model('./pkl/Lstm.h5')
-    logging.info("lstm model loaded")
+    logging.info("LSTM model loaded")
     lstm_pred = lstm_model.predict(padded_sequences)
     data['lstm_pred'] = np.argmax(lstm_pred, axis=1)
-    logging.info("data predicted")
+    logging.info("Data predicted")
     biGru_model = load_model('./pkl/biGRU.h5')
     logging.info("GRU model loaded")
     biGru_pred = biGru_model.predict(padded_sequences)
     data['biGru_pred'] = np.argmax(biGru_pred, axis=1)
-    logging.info("data predicted")
+    logging.info("Data predicted")
     biGru_model1 = load_model('./pkl/biGRU1.h5')
     logging.info("GRU model 1 loaded")
     biGru_pred1 = biGru_model1.predict(padded_sequences)
     data['biGru_pred1'] = np.argmax(biGru_pred1, axis=1)
-    logging.info("data predicted")
+    logging.info("Data predicted")
     return data
 
 
@@ -224,8 +222,10 @@ def handle_dataset(data):
     fg.to_csv('./raw_news/dailynews.csv', index=False)
     logging.info("Dataset created")
 
-
+# Main function execution
 if __name__ == "__main__":
+
+    #Building the threads to get data from all four major news channels
     money_control = threading.Thread(
         target=money_control_news, name="moneycontrol")
     mint = threading.Thread(target=mint_news, name="mint")
@@ -234,25 +234,32 @@ if __name__ == "__main__":
     economic_times = threading.Thread(
         target=economic_news, name="economic_times")
 
+    # Starting the Threads to get data from all four major news channels
     money_control.start()
     mint.start()
     business_standard.start()
     economic_times.start()
 
+    # Joining the four threads so that furthur steps run after all the 
+    # data are being extracted first
     money_control.join()
     mint.join()
     business_standard.join()
     economic_times.join()
-    news = mcnews+mnews+ecnews+bsnews
-    times = mctimes+mtimes+ectimes+bstimes
+
+    # Starting main execution
+    news = money_control_news + mint_news + economic_times_news + business_standard_news
+    times = money_control_datetimes + mint_datetimes + economic_times_datetimes + business_standard_datetimes
+
+    # Creating dataframes
     data = pd.DataFrame({'Text': news, 'Date': times})
     logging.info("dataframe created")
     data['Date'] = pd.to_datetime(data['Date']).dt.date
     data = filter_data(data)
     data = ml_model(data)
     data = dp_model(data)
-    sentimet = data.apply(calculate_sentiment, axis=1)
+    sentiment = data.apply(calculate_sentiment, axis=1)
     data = data.iloc[:, :2]
-    data['Sentiment'] = sentimet
+    data['Sentiment'] = sentiment
     logging.info("Sentiment calculated")
     handle_dataset(data)
